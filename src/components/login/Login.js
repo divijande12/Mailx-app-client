363c1ea98fb066a35a9508f698878a006a2fe4bd
divijande12 <divijande@rocketmail.com>
2021-09-12 00:00:10 +0530

    created login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { Component } from "react";
 import GoogleLogin from "react-google-login";
 import "./login.css";
+import image from "../../assets/web-bg.png";
 
 export default class Login extends Component {
   componentDidMount() {
@@ -33,21 +34,42 @@ export default class Login extends Component {
       <div className="row custom-row">
         <div
           className="col-md-6 custom-column"
-          style={{ backgroundColor: "red" }}>
-          <div>
-            <img src="/images/website_logo.png" className="img-style" alt="" />
-            <div className="header-appname">MailX</div>
-            <div className="header-tagline">This is the tagline</div>
+          style={{
+            backgroundImage: `url(${image})`,
+          }}>
+          <div style={{ textAlign: "center" }}>
+            <img src="/images/logo2.png" className="img-style" alt="" />
+            <div className="header-appname">
+              Mail<span style={{ color: "#14588F" }}>X</span>
+            </div>
+            <div className="header-tagline">
+              Send E-Mails to anyone anywhere.
+            </div>
           </div>
         </div>
         <div
           className="col-md-6 custom-column"
-          style={{ backgroundColor: "blue" }}>
+          style={{ backgroundColor: "white" }}>
           <div>
             <div className="login-msg">Sign In to send E-mails</div>
             <div className="google-button-container">
               <GoogleLogin
                 clientId="823234920920-jr3h8m5mb8ntf5apn38ipi6j1m2c6182.apps.googleusercontent.com"
+                render={(renderProps) => (
+                  <button
+                    className="google-button-container"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}>
+                    <ion-icon
+                      name="logo-google"
+                      style={{
+                        margin: "0px 5px -2px 0px",
+                        fontSize: "16px",
+                        color: "white",
+                      }}></ion-icon>
+                    Login with Google
+                  </button>
+                )}
                 buttonText="Login with Google"
                 onSuccess={this.responseGoogle}
                 onFailure={this.errorGoogle}
